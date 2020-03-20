1269f984362d885ecdac83fe7f24f6aaf8013255
Kwafo Nana Mensah (kwafoDEV) <nanamensah11@yahoo.co.uk>
2020-03-20 17:39:02 +0000

    adding Api Authentication Route

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-
+//create a  user
+Route::post('register','Api/AuthController@register');
+//create a   login
+Route::post('login','Api/AuthController@login');
 
 //List Booking
 Route::get('bookings','bookingController@index');
