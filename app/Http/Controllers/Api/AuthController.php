fa8a4449b262063f75fd351b9d55e7696e5ed533
Kwafo Nana Mensah (kwafoDEV) <nanamensah11@yahoo.co.uk>
2020-03-20 17:34:00 +0000

    login inplemnetation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,17 +6,34 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
 
+
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $validatedDate =  $request->validate([
-            'name'=>'required',
-            'email'=>'required',
+    public function register (Request $request)
+    {
+        $validatedDate = $request->validate ([
+            'name'     => 'required',
+            'email'    => 'required',
             'password' => 'required'
         ]);
-       $user= User::create($validatedDate);
-       $accessToken= $user->createToken('authToken')->accessToken;
-        
-        return response(['user'=>$user,'accessToken'=>$accessToken]);
+        $user = User::create ($validatedDate);
+        $accessToken = $user->createToken ('authToken')->accessToken;
+
+        return response (['user' => $user, 'accessToken' => $accessToken]);
+    }
+
+    public function login (Request $request)
+    {
+        $loginData = $request->validate ([
+            'email'    => 'required',
+            'password' => 'required'
+        ]);
+        if ( !auth ()->attempt ($loginData) ) {
+            return response (['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = auth ()->user ()->createToken ('authToken')->accessToken;
+
+        return response (['user' => auth ()->user (), 'accessToken' => $accessToken]);
     }
 }
