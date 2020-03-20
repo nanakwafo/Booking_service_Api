ca2c14572cd6adc9fed77393ff11378a9c7a5fda
Kwafo Nana Mensah (kwafoDEV) <nanamensah11@yahoo.co.uk>
2020-03-20 17:17:56 +0000

    register a user

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validatedDate =  $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password' => 'required'
+        ]);
+       $user= User::create($validatedDate);
+       $accessToken= $user->createToken('authToken')->accessToken;
+        
+        return response(['user'=>$user,'accessToken'=>$accessToken]);
+    }
+}
--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'passport',
             'model' => App\User::class,
         ],
 
